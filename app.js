a77b2975fe061f0c57b080e6533a1b03d431a99d
mirzamohid <16277314+mirzamohid@users.noreply.github.com>
2024-06-29 15:47:08 +0800

    setup express server

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+
+const PORT = process.env.port || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/healthcheck", (req, res, next) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
+});
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
